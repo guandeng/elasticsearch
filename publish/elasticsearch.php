44503ee005758dc981470613091835a18a3be909
guandeng <guandeng@Hotmail.com>
2023-12-02 23:03:06 +0800

    Upgrade

--- publish/elasticsearch.php
+++ publish/elasticsearch.php
@@ -1,18 +1,15 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of elasticsearch.
- *
- * @link     https://github.com/guandeng/elasticsearch
- * @document https://github.com/guandeng/elasticsearch/blob/main/README.md
- * @contact  huangdijia@gmail.com
- */
+
+use function Hyperf\Collection\value;
+use function Hyperf\Support\env;
+
 return [
     'default' => [
-        'hosts' => [
-            'http://127.0.0.1:9500',
-        ],
+        'hosts' => value(function () {
+            return explode(',', env('ELASTICSEARCH_HOSTS', ''));
+        }),
         'pool' => [
             'max_connections' => 64,
         ],
