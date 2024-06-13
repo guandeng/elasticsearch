e6735f897064302d7c0474470b425eef7fbcb23b
10947 <zx@addcn.com>
2024-06-13 11:42:42 +0800

    chore: Added configuration item for the number of retries

--- publish/elasticsearch.php
+++ publish/elasticsearch.php
@@ -1,7 +1,13 @@
 <?php
 
 declare(strict_types=1);
-
+/**
+ * This file is part of elasticsearch.
+ *
+ * @link     https://github.com/guandeng/elasticsearch
+ * @document https://github.com/guandeng/elasticsearch/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
 use function Hyperf\Collection\value;
 use function Hyperf\Support\env;
 
@@ -13,5 +19,6 @@ return [
         'pool' => [
             'max_connections' => 64,
         ],
+        'retries' => 3,
     ],
 ];
